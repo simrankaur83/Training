5f24e2a3a0df7614b6b93367cc3d1db366234deb
simran <simrankaur.june11@gmail.com>
2020-04-25 02:51:57 +0530

    first commit

---
+++ src/test/java/test/NewTest1.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.testng.annotations.Test;
+
+import tt.tt.NewTest;
+
+
+
+ class NewTest1 extends NewTest {
+	NewTest a2 = new NewTest();
+	
+  @Test
+  public void f() {
+	 
+
+	  
+  }
+  
+}
